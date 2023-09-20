73e92975af8203b76a0dd04f6c018d30cb004d84
Sergey A <sergalf81@mail.ru>
2023-09-20 11:28:25 +0300

    Написана программа к первому заданию

---
+++ C#_HW_1/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Input two numbers & the program will compare them");
+System.Console.Write("Input your first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input your second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    System.Console.WriteLine("MAX number:" + a);
+}
+else if ( a == b)
+{
+    System.Console.WriteLine("The numbers are equal ");
+}
+else
+{
+    System.Console.WriteLine("MAX number:" + b);
+}
