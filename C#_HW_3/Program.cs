a631da0b6f7266d276aabc2a0f89eccd1dd2c82b
Sergey A <sergalf81@mail.ru>
2023-09-20 12:47:53 +0300

    Добавлена програма к 3 заданию

---
+++ C#_HW_3/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Find out if number is even");
+System.Console.Write("Unput the number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    System.Console.WriteLine("The number is even");
+}
+else
+{
+    System.Console.WriteLine("The number is uneven");
+}
