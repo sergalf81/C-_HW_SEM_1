284ec47d04f006c5986891ac951878e6a654586f
Sergey A <sergalf81@mail.ru>
2023-09-20 12:36:12 +0300

    Создана программа ко 2му заданию

---
+++ C#HW_2/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.WriteLine("Input three numbers & program will compare them");
+System.Console.Write("Input your first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input your second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input your third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    if (a > c)
+    {
+        System.Console.WriteLine("MAX number: " + a);
+    }
+    else
+    {
+        System.Console.WriteLine("MAX number: " + c);
+    }
+    
+}
+    else if (b > c)
+    {
+        System.Console.WriteLine("MAX number: " + b);
+    }
+    else
+    {
+        System.Console.WriteLine("MAX number: " + c);
+    }
