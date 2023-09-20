f7f5ef22c0a0b3350897a2d950170be04338063c
Sergey A <sergalf81@mail.ru>
2023-09-20 12:56:43 +0300

    Создана программа для 4 задания

---
+++ C#_HW_4/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Find out all even numbers from 1 to n");
+System.Console.Write("Input the number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+if (i % 2 == 0)
+{
+    System.Console.WriteLine(i);
+}
